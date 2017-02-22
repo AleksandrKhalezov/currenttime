ab55d697d9e7488f966ce3eaec6f91883aee8b60
AleksandrKhalezov <remark.mark@mail.ru>
2017-02-22 15:57:44 +0300

    dev5 is not fully complete

---
+++ src/dev5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DEV_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m = string.Empty;
+            string p = string.Empty;
+            string n = string.Empty;
+            ConsoleReader reader = new ConsoleReader();
+            reader.InputMoney(m);           
+            reader.InputProductivity(p);
+            reader.InputNumberOfCriterion(n);
+            Console.ReadKey(true);
+
+        }
+    }
+}
