a9154f13cadd5518a7fe1cfccc529ada2bcfbd21
AleksandrKhalezov <remark.mark@mail.ru>
2017-03-02 15:29:19 +0300

    dev8 is complete

---
+++ src/dev8/InputMatrix.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace DEV_8
+{
+    class InputMatrix
+    {
+        public double [,] inputMatrix()
+        {
+            int a, b;
+
+            Console.WriteLine("Enter number of lines in matrix:");
+            a = EnterCorrectInt();
+
+            Console.WriteLine("Enter number of column in matrix:");
+            b = EnterCorrectInt();
+
+            double[,] matrix = new double [a, b];
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Console.WriteLine("Enter the [" + i + "][" + j + "] element");
+                    matrix[i, j] = EnterCorrectDouble();
+                    Console.Write("\n", matrix[i, j]);
+                }
+            }
+            return matrix;
+        }
+
+        public int EnterCorrectInt()
+        {
+            int number = 0;
+            string numb = string.Empty;
+            while (true)
+            {
+                numb = Console.ReadLine();
+                try
+                {
+                    number = Int32.Parse(numb);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Format. \n Enter correct integer number", numb);
+                    continue;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Overflow. \n Enter correct integer number", numb);
+                    continue;
+                }
+            }
+            return number;
+        }
+
+        public double EnterCorrectDouble()
+        {
+            double number = 0.0;
+            string numb = string.Empty;
+            while(true)
+            {
+                numb = Console.ReadLine();
+                try
+                {
+                    number = Double.Parse(numb);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Bad Format. \n Enter correct double number", numb);
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Overflow. \n Enter correct double number", numb);
+                    break;
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ src/dev8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DEV_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputMatrix inputMatrix = new InputMatrix();
+            MatrixMultyply matrixMultiply = new MatrixMultyply();
+
+            Console.WriteLine("First matrix");
+            double[,] firstMatrix = inputMatrix.inputMatrix();
+
+            Console.WriteLine("Second matrix");
+            double[,] secondMatrix = inputMatrix.inputMatrix();
+
+            double[,] resultMatrix = matrixMultiply.DoMatrixMultyply(firstMatrix,secondMatrix);
+
+            if (resultMatrix == null)
+            {
+                Console.WriteLine("We can't multiply they!");
+            }
+            else
+            {
+                for (int i = 0; i < resultMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+                    {
+                        Console.WriteLine(resultMatrix[i, j] + "    ");
+                    }
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
