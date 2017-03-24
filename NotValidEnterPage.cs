89d99c929980e69d877d4c0d297e98d5dbeb0b7d
AleksandrKhalezov <remark.mark@mail.ru>
2017-03-24 15:15:08 +0300

    Dev-13 is complete

---
+++ NotValidEnterPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace DEV_13
+{
+    [TestClass]
+    public class NotValidEnterPage
+    {
+        IWebDriver driver;
+        By errorPage = By.XPath("//*[contains(text(),'Вход')]");
+        public NotValidEnterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void FindNeedElement()
+        {
+            driver.FindElement(errorPage);
+        }
+    }
+}
